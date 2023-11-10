8d5c161d119779a28fd229552e56c679d136da37
JulianKer <jschmuker742@alumno.unlam.edu.ar>
2023-11-09 21:26:29 -0300

    Recitales, PopUp y carrousell

---
+++ js/intentoPopUp.js
@@ -0,0 +1,30 @@
+const body = document.querySelector("body");
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  const popUp = `
+    <div class="popup">
+        <div class="rectangulo">
+           <h3>Próximo recital: Artic Monkeys</h3>
+           <img src="../IMG/portadas/foto-album-artics-monkey.jpg" alt="portada" />
+           <p>¡ Últimas entradas disponibles !</p>
+           <p>Fecha: 13/9/2023</p>
+           <p>Horario: 17:30hs.</p>
+           <p class="horario">Estadio: River.</p>
+           <button class="button-login" id="btn-close-popup">Cerrar</button>
+        </div>
+    </div>
+`;
+  body.innerHTML += popUp;
+  body.style.overflow = "hidden";
+
+  console.log("popup agregado");
+
+  const btnClosePupUp = document.getElementById("btn-close-popup");
+
+  btnClosePupUp.addEventListener("click", () => {
+    body.style.overflow = "";
+
+    const popUpHtml = document.querySelector(".popup");
+    popUpHtml.style.display = "none";
+  });
+});
---
+++ js/recitales.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        arrows:true,
+        dots:true
+
+    });
+});
